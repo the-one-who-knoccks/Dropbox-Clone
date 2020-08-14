64fa59aae6943b628f4a9bbd12ccf48914807c84
Thiago Alves <thiagoalves.devp@gmail.com>
2020-08-13 22:39:17 -0300

    :sparkles: Data file

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,7 +3,7 @@ export default [
     title: 'Concentre-se apenas no trabalho importante',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis. Donec non ullamcorper sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-  },
+  }, 
   {
     title: 'Diga adeus ao trabalho maçante',
     description:
